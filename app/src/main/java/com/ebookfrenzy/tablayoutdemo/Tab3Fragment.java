4766405131cb6a007d09b367ea69bbf715b52c58
Alexx <Vexurion@gmail.com>
2020-02-24 21:23:38 -0700

    TabLayout Example Chapter 45

---
+++ app/src/main/java/com/ebookfrenzy/tablayoutdemo/Tab3Fragment.java
@@ -0,0 +1,69 @@
+package com.ebookfrenzy.tablayoutdemo;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass. Activities that contain this fragment must implement the
+ * {@link Tab3Fragment.OnFragmentInteractionListener} interface to handle interaction events.
+ */
+public class Tab3Fragment extends Fragment {
+
+  private OnFragmentInteractionListener mListener;
+
+  public Tab3Fragment() {
+    // Required empty public constructor
+  }
+
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_tab3, container, false);
+  }
+
+  // TODO: Rename method, update argument and hook method into UI event
+  public void onButtonPressed(Uri uri) {
+    if (mListener != null) {
+      mListener.onFragmentInteraction(uri);
+    }
+  }
+
+  @Override
+  public void onAttach(Context context) {
+    super.onAttach(context);
+    if (context instanceof OnFragmentInteractionListener) {
+      mListener = (OnFragmentInteractionListener) context;
+    } else {
+      throw new RuntimeException(context.toString()
+          + " must implement OnFragmentInteractionListener");
+    }
+  }
+
+  @Override
+  public void onDetach() {
+    super.onDetach();
+    mListener = null;
+  }
+
+  /**
+   * This interface must be implemented by activities that contain this fragment to allow an
+   * interaction in this fragment to be communicated to the activity and potentially other fragments
+   * contained in that activity.
+   * <p>
+   * See the Android Training lesson <a href= "http://developer.android.com/training/basics/fragments/communicating.html"
+   * >Communicating with Other Fragments</a> for more information.
+   */
+  public interface OnFragmentInteractionListener {
+
+    // TODO: Update argument type and name
+    void onFragmentInteraction(Uri uri);
+  }
+}
